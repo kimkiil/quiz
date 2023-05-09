ddf878826d6fd82b8a318296e5d47016bfd0569b
kimkiil <pqrk2468@gmail.com>
2023-05-10 00:17:49 +0900

    Update test3.java

--- test3.java
+++ test3.java
@@ -7,22 +7,26 @@ public class test3 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int s = scanner.nextInt();
-		int b = 0;
-		for(int i=2; i<s; i++) {
-			int count = 0;
-			
-			for(int j=2; j<=i; j++) {
-			if(i%j==0) {
-				count++;
-				}
-			}
-			if(count==1) {
-				b++;
-			}
+		
+		if(s<=0) {
+			System.out.println("종료");
 		}
+		else {
+			int b = 0;
+				for(int i=2; i<s; i++) {
+					int count = 0;
+			
+					for(int j=2; j<=i; j++) {
+						if(i%j==0) {
+							count++;
+						}
+					}
+					if(count==1) {
+						b++;
+					}
+				}
 		System.out.println("소수의 개수는" + b + "입니다.");
-		
-		
+		}
 		scanner.close();
 	}
 	
